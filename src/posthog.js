daa13865f1e11918da1bf9481e0524cc063444a5
Zubaid Al Jarmi <zaynzayn9696@gmail.com>
2025-12-10 11:10:04 -0800

    finished

--- src/posthog.js
+++ src/posthog.js
@@ -2,11 +2,8 @@ import posthog from 'posthog-js';
 
 export function initPostHog() {
   posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
-    api_host: import.meta.env.VITE_POSTHOG_HOST,
-    person_profiles: 'identified_only', // change to "always" if you want anonymous profiles
-    capture_pageview: true,
-    autocapture: true,
+    api_host: import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com',
   });
-}
 
-export default posthog;
+  window.posthog = posthog; // <-- critical!
+}
